12c1e2e216091be02928e7b723d1955554eb2f6a
Benjamin Pinter <ben.david.pinter@icloud.com>
2022-02-08 21:03:42 -0500

    Added keypress loop

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,26 @@
 #include "Keyboard.h"
 #include <Pinkey.cpp>
 
+int _TotalKeys;
+
 //Key Definitions
-Pinkey keys[] = {
-  {0, {KEY_LEFT_CTRL, KEY_LEFT_ALT, KEY_DELETE}},
-  {1, {KEY_LEFT_GUI, 'l'}}
+Pinkey _Keys[] = {
+    {0, {KEY_LEFT_CTRL, KEY_LEFT_ALT, KEY_DELETE}},
+    {1, {KEY_LEFT_GUI, 'l'}
+  }
 };
 
 void setup() {
+  _TotalKeys = sizeof(_Keys)/sizeof(_Keys[0]);
 }
 
 void loop() {
-
+  for(int i = 0; i < _TotalKeys; i++){
+    while(digitalRead(_Keys[i].Pin)){
+      for(int j = 0; j < _Keys[i].Keys.size(); j++){
+        Keyboard.press(_Keys[i].Keys.at(j));
+      }
+    }
+    Keyboard.releaseAll();
+  }
 }
