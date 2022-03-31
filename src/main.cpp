426e9049cfb46b81f330e898c5aa88a55c125b06
Benjamin Pinter <72715672+BenjaminDavidPinter@users.noreply.github.com>
2022-03-31 08:08:05 -0400

    Fixed bugs

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,15 @@ Configs
 
 void setup()
 {
+  /*Set all pins to default pullup*/
+  pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
+  pinMode(5, INPUT_PULLUP);
+  pinMode(6, INPUT_PULLUP);
+  pinMode(7, INPUT_PULLUP);
+  pinMode(8, INPUT_PULLUP);
+  pinMode(2, INPUT_PULLUP);
   _TotalKeys = sizeof(PinDeck_Config1) / sizeof(PinDeck_Config1[0]);
   Serial.begin(9600);
 }
@@ -20,19 +29,19 @@ void loop()
 {
   for (int i = 0; i < _TotalKeys; i++)
   {
-    while (digitalRead(PinDeck_Config1[i].Pin))
+    while (!digitalRead(PinDeck_Config1[i].Pin))
     {
       Serial.println("Key pressed: " + PinDeck_Config1[i].Pin);
       //Functionality for Chord Based Macros
       if (PinDeck_Config1->KeyType == PinkeyType::Chord)
       {
         for (int j = 0; j < MAX_STROKES; j++)
-        {
-          Serial.println("Chord key: " + PinDeck_Config1[i].Keys[j]);
-          if(PinDeck_Config1[i].Keys[j] == 0){
-            continue;
+        { 
+          if(PinDeck_Config1[i].Keys[j] != 0){
+            Serial.print("Chord Key: ");
+            Serial.println(PinDeck_Config1[i].Keys[j]);
+            Keyboard.press(PinDeck_Config1[i].Keys[j]);
           }
-          Keyboard.press(PinDeck_Config1[i].Keys[j]);
         }
       }
 
@@ -49,6 +58,7 @@ void loop()
           Keyboard.release(PinDeck_Config1[i].Keys[j]);
         }
       }
+      delay(500);
     }
     Keyboard.releaseAll();
   }
